6dabd8d9e3ec23ea5ca7d29ca78fc9b42f560bd8
Ural-viking <i-didder@yandex.ru>
2022-11-08 17:11:21 +0500

    first commit

---
+++ src/main/java/com/example/lab_4/service/StudentService.java
@@ -0,0 +1,14 @@
+package com.example.lab_4.service;
+
+import com.example.lab_4.entity.Student;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public interface StudentService {
+
+    List<Student> getAllStudents();
+    Student getStudent(int id);
+    Student saveStudent(Student student);
+    void deleteStudent(int id);
+}
